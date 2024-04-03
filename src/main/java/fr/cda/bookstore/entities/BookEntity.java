7f786856127b5c656636b560a43d439a4b0c3a73
AlexisBourges <alexis.bourges.dev@gmail.com>
2024-04-03 12:25:44 +0200

    03/04 - 12h30 :
    Création d'une entity

---
+++ src/main/java/fr/cda/bookstore/entities/BookEntity.java
@@ -0,0 +1,45 @@
+package fr.cda.bookstore.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(schema = "book")
+public class BookEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name= "id")
+    private Integer id;
+
+    @Column(name = "titre")
+    private String titre;
+
+    @Column(name = "nb_pages")
+    private Integer nbPages;
+
+    public BookEntity() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitre() {
+        return titre;
+    }
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+    public Integer getNbPages() {
+        return nbPages;
+    }
+
+    public void setNbPages(Integer nbPages) {
+        this.nbPages = nbPages;
+    }
+}
